7cb05185fb82fa028366426912a1c3995f5210ba
vagrant <vagrant@precise32.(none)>
2016-01-21 18:15:56 +0000

    javascript added

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,59 @@ $(document).ready(function() {
 	initializePage();
 })
 
-/*
- * Function that is called when the document is ready.
- */
+function projectClick(e){
+	console.log("clicked");
+ 	e.preventDefault();
+
+ 	$(this).css("background-color","green");
+ 	var projectTitle = $(this).find("p").text();
+ 	console.log(projectTitle);
+ 	var jumbotronHeader = $(".jumbotron h1");
+ 	console.log(jumbotronHeader);
+ 	jumbotronHeader.text(projectTitle);
+
+ 	$(this).find(".img").hide();
+
+ 	var containingProject = $(this).closest(".project");
+ 	var description = $(this).find(".project-description").text();
+ 	console.log(description);
+ 	if(description.length == 0)
+ 	{
+ 		containingProject.append("<div class='description'><p>Description of the Project</p></div>");
+ 	}
+ 	else
+ 	{
+ 		description.html("<p>Stop clicking me! You just did it at" + (newDate()) + "</p>");
+ 	}
+
+ 	$(this).find(".img").show();
+ }
+
+function changeWidthplusContent()
+	{
+		console.log("inside function change");
+		var projectVal = $("#project").val()
+		$(projectVal).animate(
+			{
+				width:$('#width').val()
+			});
+		var newProjectDesc = $('#description').val();
+		console.log(newProjectDesc);
+		var val = $(projectVal + " p").text();
+		console.log(val);
+		$(projectVal + " p").text(newProjectDesc);
+}
+
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
 	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("#submitBtn").click(changeWidthplusContent);
+
+	$("a.thumbnail").click(projectClick);
+
 }
+
